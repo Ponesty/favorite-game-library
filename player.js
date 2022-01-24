16b49ee67353385d6686bc0652d2144f5a9c5f0f
Jerrell Prier <jerrellprier@gmail.com>
2022-01-24 16:48:21 -0600

    Figured out how to go to game list html from the player login html. Added code in player.js

--- player.js
+++ player.js
@@ -5,6 +5,7 @@ document.querySelector('#login').onclick = () => {
     axios.get(`http://localhost:8082/player/find/Ponesty`).then(function (response){
         let player = response.data;
         console.log(player);
+        window.location.replace("http://127.0.0.1:5500/index.html");
         
     });
 }
