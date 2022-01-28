d4bab09385ba851d163fe0b3287e625d6dc6f6d4
Jerrell Prier <jerrellprier@gmail.com>
2022-01-28 10:32:18 -0600

    Tested index.js/html to see if I can display all the information from game table. Works!

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,23 +2,50 @@
 
 //let elements = document.cookie.split('=');
 console.log(document.cookie);
+const gameContainer = document.querySelector('#game-container');
 let player2 = document.cookie;
 var playerObject;
+let game;
 
-axios.get(`http://localhost:8082/player/find/${player2}`).then(function (response){
-    playerObject = response.data;
-    console.log(playerObject);
-});
+function createGameCard(game){
+    const gameCard = document.createElement('div');
+    gameCard.classList.add('game-card');
 
-document.querySelector('#get').onclick = () => {
-    axios.get(`http://localhost:8082/game/player/${playerObject.id}`).then(function (response){
-        let show2 =document.createElement('div');
-        let game = response.data[0];
-        //show2.append(game.description);
+    gameCard.innerHTML =`<h3 class="title">${game.title}</h3>
+    <img alt='Game image' src=${game.imageURL} class="game-image"/>
+    <p class="game-description">${game.description}</p>
+    <iframe width="420" height="315"
+    src=${game.videoURL}>
+    </iframe>
+    `
+
+    gameContainer.appendChild(gameCard);
+}
+
+
+
+const displayGames = (game) => {
+    gameContainer.innerHTML = ``;
+    for(let i =0; i< game.length; i++){
+        createGameCard(game[i]);
+
+    }
+}
+
+
+// axios.get(`http://localhost:8082/player/find/${player2}`).then(function (response){
+//     playerObject = response.data;
+//     console.log(playerObject);
+// });
+
+document.querySelector('#get').onclick = () => {//playerObject.id in place of 1
+    axios.get(`http://localhost:8082/game/player/1`).then(function (response){
+        //let show2 =document.createElement('div');
+        game = response.data;
         console.log(game);
-        //show1.appendChild(show2);
-        let arr = Object.values(game);
-        document.getElementById('test').innerHTML = arr;
+        displayGames(game);
+        //let arr = Object.values(game);
+        //document.getElementById('test').innerHTML = arr;
     });
 
 }
@@ -28,8 +55,8 @@ qImage = document.querySelector('#imageURL');
 qDescription = document.querySelector('#description1');
 qVideo = document.querySelector('#videoURL');
 
-document.querySelector('#post').onclick = () => {
-    axios.post(`http://localhost:8082/game/${playerObject.id}/add`,
+document.querySelector('#post').onclick = () => {//playerObject.id in place of 1
+    axios.post(`http://localhost:8082/game/1/add`,
     {
         "title": qTitle.value,
         "imageURL": qImage.value,
@@ -41,4 +68,7 @@ document.querySelector('#post').onclick = () => {
         qDescription.value ="";
         qVideo.value ="";
     });
+
+
+
 }
